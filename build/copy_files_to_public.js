cc9a042b9ce4d30b97730093d0f0ebede3d80d7d
davidhorm <davidhorm+github@gmail.com>
2018-10-10 09:28:43 -0700

    style: added filter icon, and footer that says where the icons came from

--- build/copy_files_to_public.js
+++ build/copy_files_to_public.js
@@ -6,7 +6,28 @@ var path = require("path");
 
 console.log("\n *START COPY FILES TO PUBLIC* \n");
 
-//copy favicon.ico and ./submodules/excel-bootstrap-table-filter files
+//copy static files from src
+mkdirp('./public/img', function (err) {
+    if (err) {
+        console.error(err);
+    }
+    else {
+        fs.createReadStream('./src/favicon.ico')
+        .pipe(fs.createWriteStream('./public/favicon.ico'));
+
+        var imageFiles = './src/img/*.png';
+        glob(imageFiles, null, function (err, files) {
+            files.forEach(srcFile => {
+                var destFile = srcFile.replace("/src/", "/public/");
+
+                fs.createReadStream(srcFile)
+                .pipe(fs.createWriteStream(destFile));
+            });
+        });
+    }
+});
+
+//copy submodules and node module files
 mkdirp('./public/lib/@material/button/dist', function (err) {
     if (err) {
         console.error(err);
@@ -20,9 +41,6 @@ mkdirp('./public/lib/@material/button/dist', function (err) {
 
         fs.createReadStream('./node_modules/@material/button/dist/mdc.button.min.css')
         .pipe(fs.createWriteStream('./public/lib/@material/button/dist/mdc.button.min.css'));
-
-        fs.createReadStream('./src/favicon.ico')
-        .pipe(fs.createWriteStream('./public/favicon.ico'));
     }
 });
 
